0221ea2e1ebf8f79c81a8046e619f8c1be44c7d5
Martin Schneider <martin@martins-mbp.myfiosgateway.com>
2022-06-11 17:33:57 -0400

    Rebuild with homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [done, setDone] = useState(false);
   const [f, setF] = useState();
 
   useEffect(() => {
-    setInterval(() => {
+    let i = 0;
+    const int = setInterval(() => {
       setF(Math.random());
+      i += 1;
+      if (i == 20) {
+        clearInterval(int);
+        setDone(true);
+      }
     }, 100);
   }, []);
 
   let c = [];
-  for (let i = 0; i < 100 * 1000; i++) {
+  for (let i = 0; i < (done ? 1 : 100 * 1000); i++) {
     c.push(
       <div>
         {i} - {Math.random()}
-      </div>,
+      </div>
     );
   }
 
-  return <div className="App">{c}</div>;
+  return <div className="App">{done ? null : c}</div>;
 }
 
 export default App;
